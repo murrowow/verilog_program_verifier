19054afaf8ef6ec1fef789500b26a3e483b9eee7
Sophia Zhang <szzhang@andrew.cmu.edu>
2023-12-16 03:56:09 -0500

    updated instructions on how to use

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 Install antlr4 
 
-pip install antlr4-tools
+$pip install antlr4-tools
 
 Run to generate parser and lexer files for your local machine
 
-antlr4 -Dlanguage=Python3 -visitor Verilog.g4
+$antlr4 -Dlanguage=Python3 -visitor Verilog.g4
+
+place into src your simple bit-level Verilog code
+
+you can can the verilog2z3.py file in order to produce a z3 equivalent implementation of that file
+
+$python verilog2z3.py verilog_spec=<your verilog file> 
+
+This will produce some file called <your verilog file>.py
+
+You can simply run that python file to produce your output
+
+$python <your verilog file>.py
--- verilog2z3.py
+++ verilog2z3.py
@@ -8,9 +8,9 @@ from src.VerilogParser import VerilogParser
 from src.verilogToZ3Visitor import verilogVisitor
 
 
-def preparez3(verilog_spec, verilog_spec_location):
+def z3(verilog_spec, verilog_spec_location):
 
-    filename = verilog_spec_location+verilog_spec
+    filename = verilog_spec_location + verilog_spec
     f = open(filename, "r")
     data = f.read()
     inputStream = antlr4.InputStream(data)
@@ -18,13 +18,12 @@ def preparez3(verilog_spec, verilog_spec_location):
     tokenStream = antlr4.CommonTokenStream(lexer)
     parser = VerilogParser(tokenStream)
     tree = parser.module_declaration()
-    visitor = verilogVisitor(
-        verilog_spec, verilog_spec_location)
-    z3filecontent = visitor.visit(tree)
+    visitor = verilogVisitor(verilog_spec, verilog_spec_location)
+    z3_file_content = visitor.visit(tree)
     with open('src/templateZ3Checker.py', 'r') as file:
         filedata = file.read()
         file.close()
-    filedata = filedata.replace('#*#', z3filecontent)
+    filedata = filedata.replace('#*#', z3_file_content)
     with open(verilog_spec+'.py', 'w') as file:
         file.write(filedata)
         file.close()
@@ -36,4 +35,4 @@ if __name__ == "__main__":
     parser.add_argument("--verilog_spec", type=str)
     args = parser.parse_args()
     path = 'benchmarks/'
-    preparez3(args.verilog_spec, path)
+    z3(args.verilog_spec, path)
